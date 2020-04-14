2791158a230b19836b366c36249cfa476fe95be2
neilbaldwin <neilbaldwin@mac.com>
2020-04-14 14:07:22 +0100

    more refactoring

--- delslice/__init__.py
+++ delslice/__init__.py
@@ -1,5 +1,9 @@
+#from aubio import source as source
+#from aubio import onset as onset
+
 """Top-level package for delslice."""
 
 __author__ = """Neil Baldwin"""
 __email__ = 'info@marmotaudio.co.uk'
 __version__ = '0.3'
+
--- delslice/__main__.py
+++ delslice/__main__.py
@@ -1,9 +1,105 @@
 import sys
-from aubio import source, onset
+import aubio
+#from aubio import source, onset
+
+global output_file
+#global onset_method, sourcemode_name, samplemode, samplemode_name, silence, threshold, onset_division
+#global sourceSlice, output_file, start, end, filename, output_file, sound_tail
+
+sound_tail = [
+    '\t\t\t<lfo1 type="triangle" syncLevel="0" />',
+    '\t\t\t<lfo2 type="triangle" />',
+    '\t\t\t<unison num="1" detune="8" />',
+    '\t\t\t<compressor',
+    '\t\t\t\tsyncLevel="6"',
+    '\t\t\t\tattack="327244"',
+    '\t\t\t\trelease="936" />',
+    '\t\t\t<delay',
+    '\t\t\t\tpingPong="1"',
+    '\t\t\t\tanalog="0"',
+    '\t\t\t\tsyncLevel="7" />',
+    '\t\t\t<defaultParams',
+    '\t\t\t\tarpeggiatorGate="0x00000000"',
+    '\t\t\t\tportamento="0x80000000"',
+    '\t\t\t\tcompressorShape="0xDC28F5B2"',
+    '\t\t\t\toscAVolume="0x7FFFFFFF"',
+    '\t\t\t\toscAPulseWidth="0x00000000"',
+    '\t\t\t\toscBVolume="0x80000000"',
+    '\t\t\t\toscBPulseWidth="0x00000000"',
+    '\t\t\t\tnoiseVolume="0x80000000"',
+    '\t\t\t\tvolume="0x4CCCCCA8"',
+    '\t\t\t\tpan="0x00000000"',
+    '\t\t\t\tlpfFrequency="0x7FFFFFFF"',
+    '\t\t\t\tlpfResonance="0x80000000"',
+    '\t\t\t\thpfFrequency="0x80000000"',
+    '\t\t\t\thpfResonance="0x80000000"',
+    '\t\t\t\tlfo1Rate="0x1999997E"',
+    '\t\t\t\tlfo2Rate="0x00000000"',
+    '\t\t\t\tmodulator1Amount="0x80000000"',
+    '\t\t\t\tmodulator1Feedback="0x80000000"',
+    '\t\t\t\tmodulator2Amount="0x80000000"',
+    '\t\t\t\tmodulator2Feedback="0x80000000"',
+    '\t\t\t\tcarrier1Feedback="0x80000000"',
+    '\t\t\t\tcarrier2Feedback="0x80000000"',
+    '\t\t\t\tmodFXRate="0x00000000"',
+    '\t\t\t\tmodFXDepth="0x00000000"',
+    '\t\t\t\tdelayRate="0x00000000"',
+    '\t\t\t\tdelayFeedback="0x80000000"',
+    '\t\t\t\treverbAmount="0x80000000"',
+    '\t\t\t\tarpeggiatorRate="0x00000000"',
+    '\t\t\t\tstutterRate="0x00000000"',
+    '\t\t\t\tsampleRateReduction="0x80000000"',
+    '\t\t\t\tbitCrush="0x80000000"',
+    '\t\t\t\tmodFXOffset="0x00000000"',
+    '\t\t\t\tmodFXFeedback="0x00000000">',
+    '\t\t\t\t<envelope1',
+    '\t\t\t\t\tattack="0x80000000"',
+    '\t\t\t\t\tdecay="0xE6666654"',
+    '\t\t\t\t\tsustain="0x7FFFFFD2"',
+    '\t\t\t\t\trelease="0x80000000" />',
+    '\t\t\t\t<envelope2',
+    '\t\t\t\t\tattack="0xE6666654"',
+    '\t\t\t\t\tdecay="0xE6666654"',
+    '\t\t\t\t\tsustain="0xFFFFFFE9"',
+    '\t\t\t\t\trelease="0xE6666654" />',
+    '\t\t\t\t<patchCables>',
+    '\t\t\t\t\t<patchCable',
+    '\t\t\t\t\t\tsource="velocity"',
+    '\t\t\t\t\t\tdestination="volume"',
+    '\t\t\t\t\t\tamount="0x3FFFFFE8" />',
+    '\t\t\t\t</patchCables>',
+    '\t\t\t\t<equalizer',
+    '\t\t\t\t\tbass="0x00000000"',
+    '\t\t\t\t\ttreble="0x00000000"',
+    '\t\t\t\t\tbassFrequency="0x00000000"',
+    '\t\t\t\t\ttrebleFrequency="0x00000000" />',
+    '\t\t\t</defaultParams>',
+    '\t\t\t<arpeggiator',
+    '\t\t\t\tmode="off"',
+    '\t\t\t\tnumOctaves="2"',
+    '\t\t\t\tsyncLevel="7" />',
+    '\t\t\t<modKnobs>',
+    '\t\t\t\t<modKnob controlsParam="pan" />',
+    '\t\t\t\t<modKnob controlsParam="volumePostFX" />',
+    '\t\t\t\t<modKnob controlsParam="lpfResonance" />',
+    '\t\t\t\t<modKnob controlsParam="lpfFrequency" />',
+    '\t\t\t\t<modKnob controlsParam="env1Release" />',
+    '\t\t\t\t<modKnob controlsParam="env1Attack" />',
+    '\t\t\t\t<modKnob controlsParam="delayFeedback" />',
+    '\t\t\t\t<modKnob controlsParam="delayRate" />',
+    '\t\t\t\t<modKnob controlsParam="reverbAmount" />',
+    '\t\t\t\t<modKnob controlsParam="volumePostReverbSend" patchAmountFromSource="compressor" />',
+    '\t\t\t\t<modKnob controlsParam="pitch" patchAmountFromSource="lfo1" />',
+    '\t\t\t\t<modKnob controlsParam="lfo1Rate" />',
+    '\t\t\t\t<modKnob controlsParam="pitch" />',
+    '\t\t\t\t<modKnob controlsParam="stutterRate" />',
+    '\t\t\t\t<modKnob controlsParam="bitcrushAmount" />',
+    '\t\t\t\t<modKnob controlsParam="sampleRateReduction" />',
+    '\t\t\t</modKnobs>',
+    '\t\t</sound>'
+]
 
 def main ():
-    global onset_method, sourcemode_name, samplemode, samplemode_name, silence, threshold, onset_division
-    global sourceSlice, output_file, start, end, filename, output_file, sound_tail
 
     win_s = 512  # fft size
     hop_s = win_s // 2  # hop size
@@ -82,146 +178,59 @@ def main ():
         '</kit>'
     ]
 
-    sound_tail = [
-        '\t\t\t<lfo1 type="triangle" syncLevel="0" />',
-        '\t\t\t<lfo2 type="triangle" />',
-        '\t\t\t<unison num="1" detune="8" />',
-        '\t\t\t<compressor',
-        '\t\t\t\tsyncLevel="6"',
-        '\t\t\t\tattack="327244"',
-        '\t\t\t\trelease="936" />',
-        '\t\t\t<delay',
-        '\t\t\t\tpingPong="1"',
-        '\t\t\t\tanalog="0"',
-        '\t\t\t\tsyncLevel="7" />',
-        '\t\t\t<defaultParams',
-        '\t\t\t\tarpeggiatorGate="0x00000000"',
-        '\t\t\t\tportamento="0x80000000"',
-        '\t\t\t\tcompressorShape="0xDC28F5B2"',
-        '\t\t\t\toscAVolume="0x7FFFFFFF"',
-        '\t\t\t\toscAPulseWidth="0x00000000"',
-        '\t\t\t\toscBVolume="0x80000000"',
-        '\t\t\t\toscBPulseWidth="0x00000000"',
-        '\t\t\t\tnoiseVolume="0x80000000"',
-        '\t\t\t\tvolume="0x4CCCCCA8"',
-        '\t\t\t\tpan="0x00000000"',
-        '\t\t\t\tlpfFrequency="0x7FFFFFFF"',
-        '\t\t\t\tlpfResonance="0x80000000"',
-        '\t\t\t\thpfFrequency="0x80000000"',
-        '\t\t\t\thpfResonance="0x80000000"',
-        '\t\t\t\tlfo1Rate="0x1999997E"',
-        '\t\t\t\tlfo2Rate="0x00000000"',
-        '\t\t\t\tmodulator1Amount="0x80000000"',
-        '\t\t\t\tmodulator1Feedback="0x80000000"',
-        '\t\t\t\tmodulator2Amount="0x80000000"',
-        '\t\t\t\tmodulator2Feedback="0x80000000"',
-        '\t\t\t\tcarrier1Feedback="0x80000000"',
-        '\t\t\t\tcarrier2Feedback="0x80000000"',
-        '\t\t\t\tmodFXRate="0x00000000"',
-        '\t\t\t\tmodFXDepth="0x00000000"',
-        '\t\t\t\tdelayRate="0x00000000"',
-        '\t\t\t\tdelayFeedback="0x80000000"',
-        '\t\t\t\treverbAmount="0x80000000"',
-        '\t\t\t\tarpeggiatorRate="0x00000000"',
-        '\t\t\t\tstutterRate="0x00000000"',
-        '\t\t\t\tsampleRateReduction="0x80000000"',
-        '\t\t\t\tbitCrush="0x80000000"',
-        '\t\t\t\tmodFXOffset="0x00000000"',
-        '\t\t\t\tmodFXFeedback="0x00000000">',
-        '\t\t\t\t<envelope1',
-        '\t\t\t\t\tattack="0x80000000"',
-        '\t\t\t\t\tdecay="0xE6666654"',
-        '\t\t\t\t\tsustain="0x7FFFFFD2"',
-        '\t\t\t\t\trelease="0x80000000" />',
-        '\t\t\t\t<envelope2',
-        '\t\t\t\t\tattack="0xE6666654"',
-        '\t\t\t\t\tdecay="0xE6666654"',
-        '\t\t\t\t\tsustain="0xFFFFFFE9"',
-        '\t\t\t\t\trelease="0xE6666654" />',
-        '\t\t\t\t<patchCables>',
-        '\t\t\t\t\t<patchCable',
-        '\t\t\t\t\t\tsource="velocity"',
-        '\t\t\t\t\t\tdestination="volume"',
-        '\t\t\t\t\t\tamount="0x3FFFFFE8" />',
-        '\t\t\t\t</patchCables>',
-        '\t\t\t\t<equalizer',
-        '\t\t\t\t\tbass="0x00000000"',
-        '\t\t\t\t\ttreble="0x00000000"',
-        '\t\t\t\t\tbassFrequency="0x00000000"',
-        '\t\t\t\t\ttrebleFrequency="0x00000000" />',
-        '\t\t\t</defaultParams>',
-        '\t\t\t<arpeggiator',
-        '\t\t\t\tmode="off"',
-        '\t\t\t\tnumOctaves="2"',
-        '\t\t\t\tsyncLevel="7" />',
-        '\t\t\t<modKnobs>',
-        '\t\t\t\t<modKnob controlsParam="pan" />',
-        '\t\t\t\t<modKnob controlsParam="volumePostFX" />',
-        '\t\t\t\t<modKnob controlsParam="lpfResonance" />',
-        '\t\t\t\t<modKnob controlsParam="lpfFrequency" />',
-        '\t\t\t\t<modKnob controlsParam="env1Release" />',
-        '\t\t\t\t<modKnob controlsParam="env1Attack" />',
-        '\t\t\t\t<modKnob controlsParam="delayFeedback" />',
-        '\t\t\t\t<modKnob controlsParam="delayRate" />',
-        '\t\t\t\t<modKnob controlsParam="reverbAmount" />',
-        '\t\t\t\t<modKnob controlsParam="volumePostReverbSend" patchAmountFromSource="compressor" />',
-        '\t\t\t\t<modKnob controlsParam="pitch" patchAmountFromSource="lfo1" />',
-        '\t\t\t\t<modKnob controlsParam="lfo1Rate" />',
-        '\t\t\t\t<modKnob controlsParam="pitch" />',
-        '\t\t\t\t<modKnob controlsParam="stutterRate" />',
-        '\t\t\t\t<modKnob controlsParam="bitcrushAmount" />',
-        '\t\t\t\t<modKnob controlsParam="sampleRateReduction" />',
-        '\t\t\t</modKnobs>',
-        '\t\t</sound>'
-    ]
 
     method_list = ['default', 'energy', 'hfc', 'complex', 'phase', 'specdiff', 'kl', 'mkl', 'specflux', 'divide']
 
     if len(sys.argv) < 2:
-        print("\n")
-        print("DELUGE SAMPLE 'SLICER' V0.30 by Neil Baldwin")
-        print("--------------------------------------------")
-        print("\n")
-        print("PLEASE NOTE THIS ONLY WORKS WITH DELUGE OS V3.0 AND ABOVE!")
-        print("\n")
-        print("Usage: (python) delslice [options] --input <input sample> --output <output kit>")
-        print("\n")
-        print("Run from root of your SD card!")
-        print("You must specift paths relative to the root e.g. SAMPLES/LOOP/DRUM.WAV")
-        print("\n")
-        print("Please see README.TXT for full instructions.")
-        print("\n")
-        print("REQUIRED:")
-        print("--input\t\tinput path/sample name")
-        print("--output\toutput kit path/kit name")
-        print("\n")
-        print("OPTIONS FOR TRANSIENT DETECTION:")
-        print("--method\ttransient scanning method - ENERGY, HFC (default)")
-        print("\t\tCOMPLEX, PHASE, SPECDIFF, KL, MKL, SPECFLUX")
-        print("--silence\tsilence level in DB (default -70.0)")
-        print("\t\tSets silence level in dB under which there is no detections")
-        print("\t\t-20.0 would eliminate most but the loudest signals, -90.0 would")
-        print("\t\tdetect almost everything. Default -70.0")
-        print("--threshold\tsensitivity for detection, typically 0.001 to 0.900")
-        print("\t\tLower values imply more detection, higer values detect less. Default 0.3")
-        print("\n")
-        print("OPTIONAL NUMERICAL DIVISION:")
-        print("--divide <n>\toverrides the transient detection, sets the split")
-        print("\t\tpoints <n> equally through the sample instead")
-        print("\n")
-        print("OPTIONS FOR DELUGE OUTPUT")
-        print("--sample\tdeluge sample type, CUT, ONCE, LOOP or STRETCH (default CUT)")
-        print("\n")
-        print("OPTIONS TO OUTPUT WHOLE SAMPLE AS EXTRA SLICE")
-        print("--sourceSlice\teither 'first' or 'last'")
-        print("--sourceMode\tCUT, ONCE, LOOP, STRETCH")
-        print("\n")
-        print("\n")
+        print ("""
+        
+DELUGE SAMPLE 'SLICER' V0.30 by Neil Baldwin
+--------------------------------------------
 
+PLEASE NOTE THIS ONLY WORKS WITH DELUGE OS V3.0 AND ABOVE!
+
+Usage: (python) delslice [options] --input <input sample> --output <output kit>
+
+Run from root of your SD card!
+
+You must specify paths relative to the SD root e.g. SAMPLES/LOOP/DRUM>WAV
+
+Please see README.TXT for full instructions
+
+REQUIRED:
+--input input path/sample name
+--output output kit path/kit name
+
+OPTIONS FOR TRANSIENT DETECTION:
+--method    transient scanning method - ENERGY, HFC (default)
+            COMPLEX, PHASE, SPECDIFF, KL, MKL, SPECFLUX
+            
+--silence   silence level in dB (default -70.0
+            Sets silence level in dB under which there is no detection
+            e.g. -20.0 would eliminate most but the loudest signal whereas
+            -90.0 would detect almost everything              
+
+--threshold sensitivity for detection, typically 0.001 to 0.900
+            Lower values imply more detection. Default 0.3
+   
+--divide n  overrides the transient detection and sets the split points
+            equally through the sample. 'n' sets number of slices output
+            
+OPTIONS FOR DELUGE OUTPUT:
+
+--sample    deluge sample type: CUT, ONCE, LOOP or STRETCH (default 'CUT')
+
+OPTIONS TO OUTPUT WHOLE SAMPLE AS EXTRA SLICE:
+
+--sourceSlice   either 'first' or 'last'
+--sourceMode    CUT, ONCE, LOOP or STRETCH
+
+        
+        """)
         sys.exit(1)
 
     #
-    #Process command-line arguments
+    # Process command-line arguments
     #
     for arg in range(len(sys.argv)):
         if sys.argv[arg] == '--input':
@@ -275,13 +284,13 @@ def main ():
         print("Error!")
         sys.exit(1)
 
-    #Initialisation
+    # Initialisation
     samplerate = 0
-    s = source(filename, samplerate, hop_s)
+    s = aubio.source(filename, samplerate, hop_s)
     samplerate = s.samplerate
 
-    #setup slice detection
-    o = onset(onset_method, win_s, hop_s, samplerate)
+    # setup slice detection
+    o = aubio.onset(onset_method, win_s, hop_s, samplerate)
     o.set_silence(float(silence))
     o.set_threshold(float(threshold))
 
@@ -341,14 +350,14 @@ def main ():
         start = sourceStart
         end = sourceEnd
         print("Slice {}:  {} - {}".format(sliceCount, start, end))
-        write_sound(start, end, sourcemode)
+        write_sound(start, end, sourcemode,filename,output_file)
         sliceCount += 1
 
     for slice in range(len(onsets) - 1):
         start = int(onsets[slice])
         end = int(onsets[slice + 1] - 1)
         print("Slice {}:  {} - {}".format(sliceCount, start, end))
-        write_sound(start, end, samplemode)
+        write_sound(start, end, samplemode,filename,output_file)
         sliceCount += 1
 
     if sourceSlice == 'last':
@@ -356,7 +365,7 @@ def main ():
         start = sourceStart
         end = sourceEnd
         print("Slice {}:  {} - {}".format(sliceCount, start, end))
-        write_sound(start, end, sourcemode)
+        write_sound(start, end, sourcemode, filename, output_file)
         sliceCount += 1
 
     print("\nTotal slices written to '" + kitname + "' from sample '" + filename + "': " + str(sliceCount))
@@ -372,41 +381,41 @@ def main ():
 #
 #Function: write individual sound (kit lane) to output file
 #
-def write_sound(s, e, m):
-    output_file.write('\t\t<sound\n')
-    output_file.write('\t\t\tname="SL' + str(slice) + '"\n')
-    output_file.write('\t\t\tpolyphonic="audio"\n')
-    output_file.write('\t\t\tvoicePriority="1"\n')
-    output_file.write('\t\t\tmode="subtractive"\n')
-    output_file.write('\t\t\tlpfMode="24dB"\n')
-    output_file.write('\t\t\tmodFXType="none">\n')
-    output_file.write('\t\t\t<osc1\n')
-    output_file.write('\t\t\t\ttype="sample"\n')
-    output_file.write('\t\t\t\tloopMode="' + str(m) + '"\n')
-    output_file.write('\t\t\t\treversed="0"\n')
-    output_file.write('\t\t\t\ttimeStretchEnable="0"\n')
-    output_file.write('\t\t\t\ttimeStretchAmount="0"\n')
-    output_file.write('\t\t\t\tfileName="' + filename[filename.find("SAMPLES"):] + '">\n')
-    output_file.write('\t\t\t\t<zone\n')
-    output_file.write('\t\t\t\t\tstartSamplePos="' + str(start) + '"\n')
-    output_file.write('\t\t\t\t\tendSamplePos="' + str(end) + '" />\n')
-    output_file.write("\t\t\t</osc1>\n")
+def write_sound(start, end, mode,file,output):
+    output.write('\t\t<sound\n')
+    output.write('\t\t\tname="SL' + str(slice) + '"\n')
+    output.write('\t\t\tpolyphonic="audio"\n')
+    output.write('\t\t\tvoicePriority="1"\n')
+    output.write('\t\t\tmode="subtractive"\n')
+    output.write('\t\t\tlpfMode="24dB"\n')
+    output.write('\t\t\tmodFXType="none">\n')
+    output.write('\t\t\t<osc1\n')
+    output.write('\t\t\t\ttype="sample"\n')
+    output.write('\t\t\t\tloopMode="' + str(mode) + '"\n')
+    output.write('\t\t\t\treversed="0"\n')
+    output.write('\t\t\t\ttimeStretchEnable="0"\n')
+    output.write('\t\t\t\ttimeStretchAmount="0"\n')
+    output.write('\t\t\t\tfileName="' + file[file.find("SAMPLES"):] + '">\n')
+    output.write('\t\t\t\t<zone\n')
+    output.write('\t\t\t\t\tstartSamplePos="' + str(start) + '"\n')
+    output.write('\t\t\t\t\tendSamplePos="' + str(end) + '" />\n')
+    output.write("\t\t\t</osc1>\n")
 
-    output_file.write('\t\t\t<osc2\n')
-    output_file.write('\t\t\t\ttype="sample"\n')
-    output_file.write('\t\t\t\tloopMode="0"\n')
-    output_file.write('\t\t\t\treversed="0"\n')
-    output_file.write('\t\t\t\ttimeStretchEnable="0"\n')
-    output_file.write('\t\t\t\ttimeStretchAmount="0">\n')
-    output_file.write("\t\t\t</osc2>\n")
+    output.write('\t\t\t<osc2\n')
+    output.write('\t\t\t\ttype="sample"\n')
+    output.write('\t\t\t\tloopMode="0"\n')
+    output.write('\t\t\t\treversed="0"\n')
+    output.write('\t\t\t\ttimeStretchEnable="0"\n')
+    output.write('\t\t\t\ttimeStretchAmount="0">\n')
+    output.write("\t\t\t</osc2>\n")
 
     #
     #Output 'tail' of individual sound
     #
     for l in sound_tail:
-        output_file.write(l)
-        output_file.write("\n")
+        output.write(l)
+        output.write("\n")
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
